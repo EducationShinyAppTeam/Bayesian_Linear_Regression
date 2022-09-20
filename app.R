dc2db09468a318adef863d04c6c1d13e6e96c327
neilhatfield <51502396+neilhatfield@users.noreply.github.com>
2022-09-20 10:14:59 -0400

    Changed Icon Names
    I changed the icon names for fontawesome v 0.3.0

--- app.R
+++ app.R
@@ -26,7 +26,7 @@ ui <- list(
       tags$li(
         class = "dropdown",
         tags$a(href = 'https://shinyapps.science.psu.edu/',
-               icon("home")
+               icon("house")
         )
       )
     ),
@@ -35,9 +35,9 @@ ui <- list(
       width = 250,
       sidebarMenu(
         id = "pages",
-        menuItem("Overview", tabName = "overview", icon = icon("tachometer-alt")),
+        menuItem("Overview", tabName = "overview", icon = icon("gauge-high")),
         menuItem("Prerequisites", tabName = "prerequisites", icon = icon("book")),
-        menuItem("Example", tabName = "example", icon = icon("book-reader")),
+        menuItem("Example", tabName = "example", icon = icon("book-open-reader")),
         menuItem("OLS Regression", tabName = "ols", icon = icon("wpexplorer")),
         menuItem("Bayesian Regression", tabName = "bayesian", icon = icon("wpexplorer")),
         menuItem("References", tabName = "references", icon = icon("leanpub"))
